0dc53e8a1d211cb73be90c4903ba95a9fff268b5
Shefin <shefinptr@gmail.com>
2025-09-25 13:13:46 +0530

    Setup React project with Parcel, cleaned package.json and initial structure

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Hello React 🚀
+
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compressing
+- Consistant hashing
+- Code splitting
+- Differential bundling - support older browsers
+- Diagnostics
+- Error handling
+- HTTPS
+- Tree Shaking - remove unused code
+
--- app.js
+++ app.js
@@ -1,32 +1,21 @@
-const parent = React.createElement(
-    'div', 
-    {id: "parent"}, [
-        React.createElement('div', {id: "child1"},[   
-            React.createElement('h1', {}, "Iam an h1 tag"),
-            React.createElement('h2', {}, "Iam an h2 tag"),
-            React.createElement('h3', {}, 'iam an h3 tag') 
-        ]),
-
-        React.createElement('div', {id: "child2"},[   
-            React.createElement('h1', {}, "Iam an h1 tag"),
-            React.createElement('h2', {}, "Iam an h2 tag"),
-            React.createElement('h3', {}, 'iam an h3 tag') 
-        ])
-    ]
-
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 
-const heading = React.createElement('h1', 
-    {id: "heading"}, 
-    "Hello world from React!"
+const heading = React.createElement(
+    'h1', 
+    {id: "heading"},
+    "Namaste React 🚀"
 );
 
 console.log(heading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const jsxHeading = (<h1 className="head">
+     Namaste React using JSX 🚀</h1>); 
 
-// root.render(heading);
-root.render(parent);
+console.log(jsxHeading);
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(jsxHeading);
